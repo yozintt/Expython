9c18bd8ca0e2290c08924baf14220bee0022431c
yozintt <yoanderfl@gmail.com>
2022-05-17 19:27:24 -0300

    att

---
+++ ex1.py
@@ -0,0 +1,8 @@
+A = int(input('primeiro número: '))
+B = int(input('segundo número: '))
+S = A + B
+M = A * B
+D = A / B
+print('A soma entre {} e {} vale {}'.format(A, B, S))
+print('A multiplicação entre {} e {} vale {}'.format(A, B, M))
+print('A divisão entre {} e {} vale {}'.format(A, B, D))
---
+++ ex2.py
@@ -0,0 +1,4 @@
+py = 3.14159
+R = float(input('Qual o Raio da circunferência? '))
+AREA = py * R**2
+print('A area da circunferência é igual a: ', AREA)
