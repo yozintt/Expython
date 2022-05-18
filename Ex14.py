e6f2e25e826aff3bc6fbe796752ae4d03dfa10d4
yozintt <yoanderfl@gmail.com>
2022-05-17 21:49:16 -0300

    att

--- Ex14.py
+++ Ex14.py
@@ -1,3 +1,6 @@
 B = int(input('Quantas balas serão distribuidas? '))
 P = int(input('Quantas pessoas tem para dividir? '))
-R = (B/P)
+D = int((B/P))
+R = (B%P)
+
+print('A quantidade de balas divididas é {} e a o resto {} de balas '.format(D, R))
